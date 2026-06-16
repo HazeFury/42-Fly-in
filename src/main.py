4c5b9629712bb16e275d417fad5a4f4f9303b0f0
Marc-Antoine Berger <marberge@z1r11p3.42lyon.fr>
2026-06-16 14:43:51 +0200

    added menu_view

--- src/main.py
+++ src/main.py
@@ -1,5 +1,39 @@
+import arcade
+import sys
+from views.menu_view import MenuView
+from utils.errors import CustomError
+
+
 def main() -> None:
-    print("Main function launched !")
+    print("--- STARTING THE PROGRAM ---")
+    print("Please select a card in the Arcade window...")
+
+    try:
+        window = arcade.Window(1280, 720, "Fly-in Simulation")
+
+        window.show_view(MenuView())
+
+        arcade.run()
+
+        print("\n--- PROGRAM CLOSING ---")
+
+    except KeyboardInterrupt:
+        print("\n\033[93m[INFO] User interrupt "
+              "(Ctrl+C). Program closing...\033[0m", file=sys.stderr)
+
+        if arcade.get_window():
+            arcade.close_window()
+        sys.exit(0)
+
+    except CustomError as e:
+        print(f"\n\033[91m{e}\033[0m")
+        if arcade.get_window():
+            arcade.close_window()
+        sys.exit(1)
+
+    except Exception as e:
+        print(f"\033[91m[FATAL ERROR]\033[0m {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
